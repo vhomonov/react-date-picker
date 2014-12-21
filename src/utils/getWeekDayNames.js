6b4d396c3c585772020451098659e3ed28a7072d
Elger Lambert <elger@shareworks.nl>
2014-12-21 18:42:49 +0100

    Adjust the order of week days based on the current locale

--- src/utils/getWeekDayNames.js
+++ src/utils/getWeekDayNames.js
@@ -7,11 +7,10 @@ var DEFAULT_WEEK_START_DAY = moment().startOf('week').format('d') * 1
 module.exports = function getWeekDayNames(startDay){
 
     var names = moment.weekdaysShort()
+    var index = startDay || DEFAULT_WEEK_START_DAY
 
-    if (startDay !== undefined && DEFAULT_WEEK_START_DAY != startDay){
-        var index = startDay
-
-        while (index > DEFAULT_WEEK_START_DAY){
+    if (index) {
+        while (index > 0){
             names.push(names.shift())
             index--
         }
