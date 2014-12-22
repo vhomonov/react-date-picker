a0f758c1c8b3f18a18e54fd45ed393654de7da6a
radubrehar <radu@jslog.com>
2014-12-22 15:19:19 +0200

    accept changes to fix getWeekDayNames

--- src/utils/getWeekDayNames.js
+++ src/utils/getWeekDayNames.js
@@ -9,11 +9,9 @@ module.exports = function getWeekDayNames(startDay){
     var names = moment.weekdaysShort()
     var index = startDay || DEFAULT_WEEK_START_DAY
 
-    if (index) {
-        while (index > 0){
-            names.push(names.shift())
-            index--
-        }
+    while (index > 0){
+        names.push(names.shift())
+        index--
     }
 
     return names
